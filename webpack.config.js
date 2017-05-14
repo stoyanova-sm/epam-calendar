d5b698047c42116e67c610bcd63e45d4944a4560
SVETLANA-LAPTOP\Svetlana <stoyanova.sm@gmail.com>
2017-05-14 16:47:27 +0300

    webpack.config.js improved

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,60 @@
 'use strict';
 
-const webpack = require("webpack");
-const path = require("path");
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
-	context: path.resolve("./frontend/js"),
+	context: path.resolve('./application'),
 
-	entry: "./file1.js",
+	entry: './js/file1.js',
 	output: {
-		path: path.resolve("./public"),
-		filename: "build.js"
+		path: path.resolve('./build'),
+		publicPath: '/build/',
+		filename: 'build.js'
+	},
+
+	module: {
+		rules: [{
+			test: /\.js$/,
+			include: path.resolve('./application'),
+			use: [{
+				loader: 'strict-loader'
+			}, {
+				loader: 'babel-loader?optional[]=runtime', //вспомогательный функционал babel вынесен в отдельный модуль
+				options: {
+					presets: ['es2015']
+				}
+			}]
+		}, {
+			test: /\.html$/,
+			use: ['file-loader?name=[name].[ext]', 'extract-loader', 'html-loader']
+		}, {
+			test: /\.(png|jpe?g)$/,
+			use: ['file-loader?name=[path][name].[ext]']
+		}, {
+			// Loader for less files
+			test: /\.less$/,
+			use: ExtractTextPlugin.extract({
+					use: [{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true
+						}
+					}, {
+						loader: 'postcss-loader',
+						options: {
+							sourceMap: true,
+							plugins: (loader) => require('autoprefixer')(),
+						}
+					}, {
+						loader: 'less-loader',
+						options: {
+							sourceMap: true
+						}
+					}]
+			}),
+		}]
 	},
 
 	watch: true,
@@ -18,21 +63,14 @@ module.exports = {
 		aggregateTimeout: 100
 	},
 
-	devtool: "inline-source-map",
+	devtool: 'inline-source-map',
 
 	plugins: [
-		new webpack.NoEmitOnErrorsPlugin() //не генерить файлы,  если есть ошибки в компиляции
-	],
-
-	module: {
-		loaders: [{
-			test: /\.js$/,
-			include: path.resolve("./frontend"),
-			loader: "babel-loader?optional[]=runtime", //вспомогательный функционал babel вынесен в отдельный модуль
-			query: {
-				presets: ['es2015']
-			}
-		}]
-	}
-}
-
+		new webpack.NoEmitOnErrorsPlugin(), //не генерить файлы,  если есть ошибки в компиляции
+		new ExtractTextPlugin({
+			filename: 'style.css',
+			disable: false,
+			allChunks: true
+		})
+	]
+};
