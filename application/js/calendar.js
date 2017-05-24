0176d3d64c17237c2b27e5dcf0ac8fb36814bc41
SVETLANA-LAPTOP\Svetlana <stoyanova.sm@gmail.com>
2017-05-24 20:52:14 +0300

    no message

--- application/js/calendar.js
+++ application/js/calendar.js
@@ -3,14 +3,16 @@ import '../data/tasksObject.json';
 export default class Calendar {
 	constructor() {
 		this.displayDate = new Date();
+		this.calendarMap = [];
 	};
 
-	initialize() {
+	initialize() { //draw the calendar and add events to switch months
 		this.setEvents();
 		this.draw();
 	};
 
-	setEvents() {
+	setEvents() { //add events to switch months by clicking arrow buttons
+		///TODO: change <a> to <button>
 		document.querySelector('.back').addEventListener('click', () => this.switchMonth(true));
 		document.querySelector('.forward').addEventListener('click', () => this.switchMonth(false));
 	};
@@ -33,20 +35,20 @@ export default class Calendar {
 
 	setHeader() {
 		const header = document.querySelector('.calendar-header');
-		const monthAndYear = this.displayDate.toLocaleString('en-EN',{
+		const options = {
 			year: 'numeric',
 			month: 'long'
-		});
-
+		}
+		const monthAndYear = this.displayDate.toLocaleString('en-EN', options);
 		header.innerText = monthAndYear;
 	};
 
 	generateDates() {
-		let firstDateToDisplay = new Date(this.displayDate.getTime());
+		const firstDateToDisplay = new Date(this.displayDate.getTime());
 		let firstWeekdayOfMonth;
 		const monthLength = new Date(firstDateToDisplay.getFullYear(), firstDateToDisplay.getMonth()+1, 0).getDate();
 
-		firstDateToDisplay.setHours(0,0,0,0);
+		firstDateToDisplay.setHours(0,0,0,0);///TODO: think about this
 		firstDateToDisplay.setDate(1); // If Monday is the first day of the Month
 		firstWeekdayOfMonth = firstDateToDisplay.getDay();
 
@@ -58,12 +60,10 @@ export default class Calendar {
 			this.setLastMondayOfPreviousMonth(firstDateToDisplay);
 		}
 
-		this.calendarMap = [];
-
 		for(let i = 0; i < 35; i++) {
 			const dateObj = new Date(firstDateToDisplay.getTime());
 			const date = dateObj.getDate();
-			const dateTime = dateObj.getTime();
+			const dateTime = dateObj.getTime();///TODO: think about this
 
 			this.calendarMap.push({dateObj, dateTime, date});
 			firstDateToDisplay.setDate(date + 1);
@@ -76,22 +76,22 @@ export default class Calendar {
 		date.setDate(lastMondayOfPreviousMonth);
 	};
 
-	requestTasks() {
+	requestTasks() { ///TODO: add catch errors,  json()
 		fetch('/build/data/tasksObject.json')
 			.then(response => {
 				if(response.status === 200) {
 					return response.json();
 				}
 			})
-			.then(tasks => this.updateCalendarMap(tasks))
+			.then(tasksList => this.updateCalendarMap(tasksList))
 			.then(() => this.render());
 	};
 
 	updateCalendarMap(tasksByDates) {
 		for(const tasksByDate of tasksByDates) {
+			// check if time in each day in calendar equals to time
+			//in every object, we received from server and returns this object from calendarMap
 			const calendarDate = this.calendarMap.find(calendarMapDay => calendarMapDay.dateTime === tasksByDate.dateTime);
-
-			calendarDate.tasks = tasksByDate.tasks;
 			if (calendarDate) {
 				calendarDate.tasks = tasksByDate.tasks;
 			}
@@ -114,18 +114,29 @@ export default class Calendar {
 			calendarHtml.push(template);
 		}
 
-		calendar.innerHTML = calendarHtml.join('');
+		calendar.innerHTML = calendarHtml.join(''); //combine the array of templates into a string
 	};
 
 	getTasksHtml(element) {
-		const template = [];
+		const htmlOutput = [];
 
-		if (!element.tasks) {
+		if (!element.tasks) { //if no task do nothing
 			return '';
 		}
 
-		for (let task of element.tasks) {
-			template.push(
+		const tasksLength = element.tasks.length;
+		for (let i = 0, task = element.tasks[i]; i < tasksLength; i++) { //go through the tasks in array
+			if(i === 4 && tasksLength > 5) {
+				htmlOutput.push(
+					`<div class="task task-duration-1 task-more">
+						<div class="task-contents">
+							<a href="#">${tasksLength - 4} more items</a> 
+						</div>
+					</div>`
+				);
+			}
+
+			htmlOutput.push(
 				`<div class="task task-duration-${task.duration} task-color-${task.color}">
 					<div class="task-label"></div>
 					<div class="task-contents">
@@ -134,105 +145,20 @@ export default class Calendar {
 				</div>`
 			);
 		}
+		this.calcDurationHtml();
+		return htmlOutput.join('');
+	}
 
-		return template.join('');
+	calcDurationHtml() {
+		console.log(this.calendarMap);
+		for(let i = 0; i < 35; i++) {
+			let a = this.calendarMap[1].dateObj.getDay();
+			console.log(a);
+			for (const tasks of this.calendarMap) {
+
+			}
+		}
+
 	}
 };
 
-/*
-let currentDate = new Date();
-
-function setCalendarHeader() {
-	const options = {year: 'numeric', month: 'long'};
-	/// TODO: add a determine locale function
-	const monthYear = currentDate.toLocaleString('en-EN', options);
-	const header = document.querySelector('.calendar-header');
-	header.innerText = monthYear;
-}
-
-function switchDate(direction) {
-	if(direction) {
-		currentDate.setMonth(currentDate.getMonth() - 1);
-	} else {
-		currentDate.setMonth(currentDate.getMonth() + 1);
-	}
-
-	setCalendarHeader();
-	generateDates();
-}
-
-function getLastMondayOfPreviousMonth(date) { //returns Date() with the date of last monday of previous month
-	date.setDate(0); // last day of previous month
- 	const lastMondayOfPreviousMonth = date.getDate() - (date.getDay() - 1);
-	date.setDate(lastMondayOfPreviousMonth);
-	return date;
-}
-
-function getArrayOfDates(firstDate, length) {
-	const arrayOfDates = [];
-	for(let i = 0; i < length; i++) {
-		arrayOfDates.push(firstDate++)
-	}
-	if (length < 28) {
-		const options = {month: 'short'};
-		const date = new Date(currentDate.getTime());
-		if (arrayOfDates[0] === 1) {
-			date.setMonth(date.getMonth()+1);
-			const localMonth = date.toLocaleString('en-EN', options);
-			arrayOfDates[0] = `${localMonth} 1`;
-		} else {
-			date.setMonth(date.getMonth()-1);
-			const localMonth = date.toLocaleString('en-EN', options);
-			arrayOfDates[length-1] = `${localMonth} ${arrayOfDates[length-1]}`;
-		}
-	}
-	return arrayOfDates;
-}
-
-function generateDates() {
-	const firstDay = new Date(currentDate.getTime());
-	firstDay.setDate(1);
-	const firstWeekdayOfMonth = firstDay.getDay();
-	const monthLength = new Date (firstDay.getFullYear(), firstDay.getMonth()+1, 0).getDate();
-	console.log(2, monthLength);
-
-	let prevMonth = [];
-	let thisMonth = [];
-	let nextMonth = [];
-
-	if (firstWeekdayOfMonth  === 6 && monthLength === 31) { // If Saturday and 31 days
-		thisMonth = getArrayOfDates(3, monthLength - 2);
-	} else	if (firstWeekdayOfMonth === 0) {
-		thisMonth = getArrayOfDates(2, monthLength - 1);
-	} else {
-		thisMonth = getArrayOfDates(1, monthLength);
-		if(firstWeekdayOfMonth !== 1) {
-			const prevFirstDate = getLastMondayOfPreviousMonth(firstDay);
-			prevMonth = getArrayOfDates(prevFirstDate.getDate(), firstWeekdayOfMonth-1 );
-		}
-	}
-
-	nextMonth  = getArrayOfDates(1, 35 - prevMonth.length - thisMonth.length) //because there are 35 cells
-
-	const dates = prevMonth.concat(thisMonth, nextMonth);
-
-	insertDates(dates);
-}
-
-function insertDates(array) {
-	const dateElements = document.querySelectorAll('.date');
-
-	for (let i = 0; i < dateElements.length; i++) {
-		const value = array[i];
-		dateElements[i].innerText = value;
-	}
-}
-
-document.addEventListener('DOMContentLoaded', () => {
-	setCalendarHeader();
-	generateDates();
-	///TODO: what about page reload
-	document.querySelector('.back').addEventListener('click', () => switchDate(true));
-	document.querySelector('.forward').addEventListener('click', () => switchDate(false));
-});
-*/
