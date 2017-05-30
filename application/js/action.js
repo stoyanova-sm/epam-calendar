08fadeb8e018bdad123eb3c09442c85edfef7d2b
SVETLANA-LAPTOP\Svetlana <stoyanova.sm@gmail.com>
2017-05-31 00:26:55 +0300

    nav actions improved

--- application/js/action.js
+++ application/js/action.js
@@ -9,16 +9,10 @@ function controlAction () {
 function navAction() {
 	const nav = document.querySelector('.nav');
 	const show = document.querySelector('.show');
-	const hide = document.querySelector('.hide');
+	const calendar = document.querySelector('main');
 	show.addEventListener('click', () => {
-		nav.classList.add('extended');
-		show.style.display = 'none';
-		hide.style.display = 'block';
-	});
-	hide.addEventListener('click', () => {
-		nav.classList.remove('extended');
-		hide.style.display = 'none';
-		show.style.display = 'block';
+		nav.classList.toggle('extended');
+		calendar.classList.toggle('extended');
 	})
 }
 
