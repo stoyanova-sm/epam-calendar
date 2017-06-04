eda8787287a162c3365884bafc9c05e5cd5f5186
SVETLANA-LAPTOP\Svetlana <stoyanova.sm@gmail.com>
2017-06-05 01:25:49 +0300

    added babel-loader for vue, removed comments

--- webpack.config.js
+++ webpack.config.js
@@ -15,17 +15,14 @@ module.exports = {
 	module: {
 		rules: [{
 			test: /\.vue$/,
-			loader: 'vue-loader'
-			// options: {
-			//   loaders: {
-			//     // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
-			//     // the "scss" and "sass" values for the lang attribute to the right configs here.
-			//     // other preprocessors should work out of the box, no loader config like this nessessary.
-			//     //'scss': 'vue-style-loader!css-loader!sass-loader',
-			//     //'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax'
-			//   }
-			//   // other vue-loader options go here
-			// }
+			use: [{
+				loader: 'vue-loader',
+				options: {
+					loaders: {
+						js: 'babel-loader?presets[]=es2015'
+					}
+				}
+			}],
 		}, {
 			test: /\.js$/,
 			include: path.resolve('./application'),
@@ -101,38 +98,6 @@ module.exports = {
 			}]
 		}
 	}
-	//,
-	// resolve: {
-	//   alias: {
-	//     'vue$': 'vue/dist/vue.common.js'
-	//   }
-	// },
-	// devServer: {
-	//   historyApiFallback: true,
-	//   noInfo: true
-	// },
-	// performance: {
-	//   hints: false
-	// },
 };
 
-// if (process.env.NODE_ENV === 'production') {
-//   module.exports.devtool = '#source-map'
-//   // http://vue-loader.vuejs.org/en/workflow/production.html
-//   module.exports.plugins = (module.exports.plugins || []).concat([
-//     new webpack.DefinePlugin({
-//       'process.env': {
-//         NODE_ENV: '"production"'
-//       }
-//     }),
-//     new webpack.optimize.UglifyJsPlugin({
-//       sourceMap: true,
-//       compress: {
-//         warnings: false
-//       }
-//     }),
-//     new webpack.LoaderOptionsPlugin({
-//       minimize: true
-//     })
-//   ])
-// }
+
