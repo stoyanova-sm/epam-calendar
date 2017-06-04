e0f6037435010bbac3eaa4883043940d0d066d03
SVETLANA-LAPTOP\Svetlana <stoyanova.sm@gmail.com>
2017-06-05 01:16:34 +0300

    added events to control section

--- application/App.vue
+++ application/App.vue
@@ -3,43 +3,14 @@
 		<div class="main-content" :class="{'extended-left': extendedLeft, 'extended-right': extendedRight}">
 			<nav-menu></nav-menu>
 			<router-view :key="$route.path"></router-view>
-
-			<section class="control">
-				<h1>On control</h1>
-				<section class="list">
-					<div>
-						<div class="list-number">01-01</div>
-						<div class="list-type">Letter</div>
-						<div class="list-content">Whatever</div>
-					</div>
-					<div>
-						<div  class="list-number">01-01</div>
-						<div  class="list-type">Letter</div>
-						<div  class="list-content">Whatever</div>
-					</div>
-					<div>
-						<div  class="list-number">01-02</div>
-						<div  class="list-type">Request</div>
-						<div  class="list-content">Gimme Fuel</div>
-					</div>
-					<div>
-						<div  class="list-number">01-02</div>
-						<div  class="list-type">Request</div>
-						<div  class="list-content">Gimme Fuel</div>
-					</div>
-					<div>
-						<div  class="list-number">01-02</div>
-						<div  class="list-type">Request</div>
-						<div  class="list-content">Gimme Fuel</div>
-					</div>
-				</section>
-			</section>
+			<control></control>
 		</div>
 	</div>
 </template>
 
 <script>
 	import Nav from './Nav.vue';
+	import Control from './Control.vue';
 
 	export default {
 		name: 'app',
@@ -58,7 +29,8 @@
 			});
 		},
 		components: {
-			'nav-menu': Nav
+			'nav-menu': Nav,
+			'control': Control
 		}
 	}
 
---
+++ application/Control.vue
@@ -0,0 +1,61 @@
+<template>
+    <section v-on:click="expandControl" :class="{'control-extended': showControl}" class="control">
+        <h1>On control</h1>
+        <section class="list">
+            <div>
+                <div class="list-number">01-01</div>
+                <div class="list-type">Letter</div>
+                <div class="list-content">Whatever</div>
+            </div>
+            <div>
+                <div  class="list-number">01-01</div>
+                <div  class="list-type">Letter</div>
+                <div  class="list-content">Whatever</div>
+            </div>
+            <div>
+                <div  class="list-number">01-02</div>
+                <div  class="list-type">Request</div>
+                <div  class="list-content">Gimme Fuel</div>
+            </div>
+            <div>
+                <div  class="list-number">01-02</div>
+                <div  class="list-type">Request</div>
+                <div  class="list-content">Gimme Fuel</div>
+            </div>
+            <div>
+                <div  class="list-number">01-02</div>
+                <div  class="list-type">Request</div>
+                <div  class="list-content">Gimme Fuel</div>
+            </div>
+        </section>
+    </section>
+</template>
+
+<script>
+    export default {
+        name: 'control',
+		data() {
+			return {
+				showControl: false
+			}
+		},
+		methods: {
+			expandControl() {
+				this.showControl = !this.showControl;
+				this.$root.$emit('expandControl');
+			}
+		},
+    };
+</script>
+
+<style scoped>
+    img {
+        float: left;
+        width: 300px;
+    }
+    li {
+        margin-bottom: 40px;
+        clear: both;
+    }
+</style>     
+
